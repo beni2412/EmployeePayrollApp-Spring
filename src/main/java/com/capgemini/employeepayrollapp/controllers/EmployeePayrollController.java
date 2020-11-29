04ad4358821e8d46575b956bf6eded4be0e82539
beni2412 <hardaman97@gmail.com>
2020-11-30 01:56:49 +0530

    [Hardaman] Added:Service layer for object creation using AutoWired annotations

--- src/main/java/com/capgemini/employeepayrollapp/controllers/EmployeePayrollController.java
+++ src/main/java/com/capgemini/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -1,5 +1,8 @@
 package com.capgemini.employeepayrollapp.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,48 +17,51 @@ import org.springframework.web.bind.annotation.RestController;
 import com.capgemini.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.capgemini.employeepayrollapp.dto.ResponseDTO;
 import com.capgemini.employeepayrollapp.model.EmployeePayrollData;
+import com.capgemini.employeepayrollapp.service.IEmployeePayrollService;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
+	@Autowired
+	private IEmployeePayrollService employeePayrollService;
 
 	@RequestMapping(value = { "", "/", "/get" })
 	public ResponseEntity<ResponseDTO> getEmployeePayrollData() {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1,new EmployeePayrollDTO("Hardaman",500000));
-		ResponseDTO respDTO = new ResponseDTO("Get call for Success ",empData);
+		List<EmployeePayrollData> empDataList = null;
+		empDataList = employeePayrollService.getEmployeePayrollData();
+		ResponseDTO respDTO = new ResponseDTO("Get call for Success ", empDataList);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
 	@GetMapping("/get/{empId}")
 	public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId") int empId) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1,new EmployeePayrollDTO("Hardaman",500000));
-		ResponseDTO respDTO = new ResponseDTO("Get Call success for ID  ",empData);
+		EmployeePayrollData empData =null;
+		empData =employeePayrollService.getEmployeePayrollDataById(empId);
+		ResponseDTO respDTO = new ResponseDTO("Get Call success for ID  ", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
 	@PostMapping("/create")
 	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1,empPayrollDTO);
-		ResponseDTO respDTO = new ResponseDTO("Created EmployeePayroll Data Success",empData);
+		EmployeePayrollData empData=null;
+		empData =employeePayrollService.createEmployeePayrollData(empPayrollDTO);
+		ResponseDTO respDTO = new ResponseDTO("Created EmployeePayroll Data Success", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
 	@PutMapping("/update")
 	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1,empPayrollDTO);
-		ResponseDTO respDTO = new ResponseDTO("Updated EmployeePayroll Data Success  ",empData);
+		EmployeePayrollData empData=null;
+		empData =employeePayrollService.updateEmployeePayrollData(empPayrollDTO);
+		ResponseDTO respDTO = new ResponseDTO("Updated EmployeePayroll Data Success  ", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete/{empId}")
 	public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
-		ResponseDTO respDTO = new ResponseDTO("Deleted Successfully", "Deleted Id "+empId);
+		employeePayrollService.deleteEmployeePayrollData(empId);
+		ResponseDTO respDTO = new ResponseDTO("Deleted Successfully", "Deleted Id " + empId);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
 }
-
---
+++ src/main/java/com/capgemini/employeepayrollapp/service/IEmployeePayrollService.java
@@ -0,0 +1,20 @@
+package com.capgemini.employeepayrollapp.service;
+
+
+
+import java.util.List;
+
+import com.capgemini.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.capgemini.employeepayrollapp.model.EmployeePayrollData;
+
+public interface IEmployeePayrollService {
+	List<EmployeePayrollData> getEmployeePayrollData();
+
+	EmployeePayrollData getEmployeePayrollDataById(int empId);
+
+	EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+
+	EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+
+	void deleteEmployeePayrollData(int empId);
+}
